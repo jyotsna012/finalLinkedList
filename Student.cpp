897afe3f782343478b9c3c74805e6b9a787183cc
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-01-31 12:04:18 -0800

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -1,22 +1,47 @@
 #include "Student.h"
 #include <cstring>
 
-Student::Student(char* nName){
-
-  name = new char[20];
-  strcpy(name,nName);
+Student::Student(){
 
 }
 
+void Student::setFName(char* nName){
+  firstName = new char[20];
+  strcpy(firstName,nName);
+}
+
+void Student::setLName(char* nName){
+  lastName = new char[20];
+  strcpy(lastName,newName);
+}
+
+void Student::setID(int nID){
+  studentId = nID;
+}
+
+void Student::setGPA(float newGPA){
+  GPA = newGPA;
+}
+
+char* Student::getFName(){
+  return firstName;
+}
+
+char* Student::getLName(){
+  return lastName;
+}
+
+int Student::getID(){
+  return studentId;
+}
+
+float Student::getGPA(){
+  return GPA;
+}
 
 Student::~Student(){
 
-  delete name;
+  delete firstName;
+  delete lastName;
   
-}
-
-char* Student::getName(){
-
-  return name;
-
 }
