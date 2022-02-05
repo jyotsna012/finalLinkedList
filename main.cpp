c80192c853785d1d2a5e37c32432d9c32dbdc8d6
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-02-05 09:15:04 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,6 +14,28 @@ void DELETE(Node*& head, Node* current, Node* prev, int ID);
 int main(){
   
   Node* head = NULL;
+  Node* next = NULL;
+  Node* next2 = NULL;
+
+  head -> setNext(next);
+  next -> setNext(next2);
+  next2 -> setNext(NULL);
+	
+  head->getStudent()->setFName("Josie");
+  head->getStudent()->setLName("Tera");
+  head->getStudent()->setID("405456");
+  head->getStudent()->setGPA("4.987");
+
+  next->getStudent()->setFName("Josie1");
+  next->getStudent()->setLName("Tera1");
+  next->getStudent()->setID("4054561");
+  next->getStudent()->setGPA("4.9871");
+	
+  next2->getStudent()->setFName("Josie12");
+  next2->getStudent()->setLName("Tera12");
+  next2->getStudent()->setID("40545612");
+  next2->getStudent()->setGPA("4.98712");
+	
   PRINT(head);
 
 }
